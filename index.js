9d3a7c0b5cd655d11c8cebd49f0d59e3af408392
Raumeco <raulmelco@gmail.com>
2023-04-02 12:29:26 +0200

    Get categorias palabras and insert both

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAPTTf0XBGT8vgbzrGN3qxR6NHiiMIMZJk",
@@ -21,3 +21,82 @@ const db = getFirestore(app)
 } catch (e) {
     console.error("Error adding document: ", e);
 } */
+
+async function getPalabras() {
+    let palabras = [];
+    const querySnapshot = await getDocs(collection(db, "palabras"));
+    querySnapshot.forEach((doc) => {
+        palabras.push({
+            id: doc.id,
+            value: doc.data().value
+        });
+    });
+}
+
+getCategorias()
+getPalabras()
+
+var categorias = []
+
+async function getCategorias() {
+    document.getElementById("selectCategorias").innerHTML = ""
+    const querySnapshot = await getDocs(collection(db, "categorias"));
+    querySnapshot.forEach((doc) => {
+        let nombre = doc.data().name
+        let id = doc.id
+        categorias.push({
+            id: id,
+            name: nombre
+        })
+        document.getElementById("selectCategorias").innerHTML += `<option class="categoriaOption" value="${id}">${nombre}</option>`
+    });
+}
+
+document.getElementById("newCategoria").addEventListener("click", async function(e) {
+    let name = document.getElementById("inputCategoria").value;
+    try {
+        let found = false;
+        categorias.forEach(element => {
+            if(element.name.toLowerCase() === name.toLowerCase()) {
+                found = true;
+            }
+        });
+
+        if (found) {
+            document.getElementById("inputCategoria").value = ""
+            return
+        };
+        
+        const docRef = await addDoc(collection(db, "categorias"), {
+            name: name
+        });
+        
+        
+        categorias.push({
+            id: docRef.id,
+            name: name
+        });
+
+        document.getElementById("selectCategorias").innerHTML += `<option class="categoriaOption" value="${docRef.id}">${name}</option>`
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+    document.getElementById("inputCategoria").value = ""
+});
+
+
+document.getElementById("newPalabra").addEventListener("click", async function(e) {
+    let name = document.getElementById("inputPalabra").value;
+    let categoria = document.getElementById("selectCategorias").value;
+
+    try {        
+        const docRef = await addDoc(collection(db, "palabras"), {
+            value: name,
+            categoria: categoria
+        });
+
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+    document.getElementById("inputPalabra").value = ""
+});
